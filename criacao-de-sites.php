2cb5718c699364c14a95a8ef02d1821df001f4ab
Henrique-Guido <henriqueguido.006@gmail.com>
2024-07-29 14:46:34 -0300

    página criação de sites

--- criacao-de-sites.php
+++ criacao-de-sites.php
@@ -1,10 +1,108 @@
 <?php include_once("header.php"); ?>
 
-    <div class="container mt-100 mt-60">
-        <div class="row">
-            <h3 class="mt-4 pt2">Por que minha empresa precisa de um site profissional? </h3>
-            <p class="text-mutted">Ah, ter um site profissional para sua empresa é fundamental nos dias de hoje. É a vitrine digital da sua marca, sua representação online, a manifestação digital do seu negócio no vasto oceano da internet. É o seu bilhete dourado para alcançar um público global e mostrar ao mundo o quão sério e competente é o seu trabalho. Afinal, o site é o seu ultimate braço direito no mundo digital!</p>
+<div class="container mt-100 mt-60">
+    <div class="row">
+        <div class="col-10" mt-100>
+            <h3 class="mt-5 mb-2 h2 text-primary">Por que minha empresa precisa de um site profissional?</h3>
+            <p class="text-secondary">Ah, ter um site profissional para sua empresa é fundamental nos dias de hoje. É a vitrine digital da sua marca, sua representação online, a manifestação digital do seu negócio no vasto oceano da internet. É o seu bilhete dourado para alcançar um público global e mostrar ao mundo o quão sério e competente é o seu trabalho. Afinal, o site é o seu ultimate braço direito no mundo digital!</p>
         </div>
     </div>
+    <div class="row">
+        <div class="col-10">
+            <h3 class="mt-5 mb-2 h2 text-primary">Como é o processo de desenvolvimento de site profissional?</h3>
+            <p class="text-secondary">O processo de desenvolvimento de um site profissional geralmente envolve várias etapas fundamentais para garantir que o resultado final atenda aos objetivos da empresa e às expectativas dos usuários. Aqui está um resumo geral do processo:</p>
+            <ul>
+                <li class="list-group-item border-0 mb-3"><p class="h5 mt-2 text-primary">Planejamento e Estratégia:</p>
+                    <ul class="list-group">
+                        <li class="list-group-item border-0 text-secondary">Definição dos objetivos do site (ex: aumentar vendas, fortalecer a marca, fornecer informações, etc.).</li>
+                        <li class="list-group-item border-0 text-secondary">Identificação do público-alvo e análise de suas necessidades.</li>
+                        <li class="list-group-item border-0 text-secondary">Elaboração de um plano de conteúdo e estrutura do site.</li>
+                    </ul>
+                </li>
+                <li class="list-group-item border-0 mb-3"><p class="h5 mt-2 text-primary">Design:</p>
+                    <ul class="list-group">
+                        <li class="list-group-item border-0 text-secondary">Criação do design visual do site, incluindo layout, cores, tipografia, e elementos gráficos.</li>
+                        <li class="list-group-item border-0 text-secondary">Desenvolvimento de wireframes ou protótipos para visualização e teste de usabilidade.</li>
+                    </ul>
+                </li>
+                <li class="list-group-item border-0 mb-3"><p class="h5 mt-2 text-primary">Desenvolvimento Front-end:</p>
+                    <ul class="list-group">
+                        <li class="list-group-item border-0 text-secondary">Codificação da interface do usuário usando linguagens como HTML, CSS, JavaScript.</li>
+                        <li class="list-group-item border-0 text-secondary">Garantia de que o site seja responsivo, ou seja, que se adapte bem a dispositivos móveis e desktops.</li>
+                    </ul>
+                </li>
+                <li class="list-group-item border-0 mb-3"><p class="h5 mt-2 text-primary">Desenvolvimento Back-end (se necessário):</p>
+                    <ul class="list-group">
+                        <li class="list-group-item border-0 text-secondary">Implementação de funcionalidades mais complexas, como bancos de dados, sistemas de gerenciamento de conteúdo (CMS), integração de sistemas, etc.</li>
+                    </ul>
+                </li>
+                <li class="list-group-item border-0 mb-3"><p class="h5 mt-2 text-primary">Testes:</p>
+                    <ul class="list-group">
+                        <li class="list-group-item border-0 text-secondary">Verificação de todos os aspectos do site, incluindo funcionalidades, navegabilidade, desempenho e segurança.</li>
+                        <li class="list-group-item border-0 text-secondary">Testes de compatibilidade com diferentes navegadores e dispositivos.</li>
+                    </ul>
+                </li>
+                <li class="list-group-item border-0 mb-3"><p class="h5 mt-2 text-primary">Lançamento:</p>
+                    <ul class="list-group">
+                        <li class="list-group-item border-0 text-secondary">Publicação do site no servidor de hospedagem após todos os testes e revisões estarem concluídos.</li>
+                        <li class="list-group-item border-0 text-secondary">Configuração de domínio e otimização para mecanismos de busca (SEO).</li>
+                    </ul>
+                </li>
+                <li class="list-group-item border-0 mb-3"><p class="h5 mt-2 text-primary">Manutenção contínua:</p>
+                    <ul class="list-group">
+                        <li class="list-group-item border-0 text-secondary">Atualização regular do conteúdo e manutenção técnica para garantir que o site continue funcionando sem problemas e esteja seguro contra ameaças online.</li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-10">
+            <h3 class="mt-5 mb-2 h2 text-primary">Quanto custa ter um site profissional de alta performance?</h3>
+            <p class="text-secondary">O custo de um site profissional de alta performance pode variar significativamente dependendo de vários fatores, como complexidade do design, funcionalidades requeridas, tamanho da equipe de desenvolvimento, localização geográfica dos desenvolvedores, entre outros. Aqui estão alguns pontos a considerar:</p>
+            <ul class="list-group">
+                <li class="list-group-item border-0 mb-2 text-secondary"><span class="text-primary font-weight-bold">Design e Desenvolvimento:</span> O custo inicial pode variar de algumas centenas a milhares de dólares, dependendo do design personalizado, da quantidade de funcionalidades interativas, e da experiência dos desenvolvedores.</li>
+                <li class="list-group-item border-0 mb-2 text-secondary"><span class="text-primary font-weight-bold">Funcionalidades Adicionais:</span> Integrar sistemas de pagamento, sistemas de gerenciamento de conteúdo robustos, ou funcionalidades de comércio eletrônico pode aumentar o custo.</li>
+                <li class="list-group-item border-0 mb-2 text-secondary"><span class="text-primary font-weight-bold">SEO e Marketing:</span> Investimentos em otimização para mecanismos de busca (SEO), campanhas de marketing digital, e análise de desempenho podem ser necessários para garantir que o site atinja seu público-alvo.</li>
+                <li class="list-group-item border-0 mb-2 text-secondary"><span class="text-primary font-weight-bold">Manutenção e Atualizações:</span> Considerar os custos contínuos de hospedagem, atualizações de segurança, e manutenção técnica para garantir que o site continue funcionando sem problemas.</li>
+                <li class="list-group-item border-0 mb-2 text-secondary"><span class="text-primary font-weight-bold">Escopo do Projeto:</span> Projetos maiores, que exigem mais tempo de desenvolvimento e recursos, geralmente têm um custo maior.</li>
+            </ul>
+            <p class="text-secondary">Para ter uma ideia mais precisa do custo, é recomendável obter orçamentos de várias empresas de desenvolvimento web. Além disso, é importante considerar o retorno sobre o investimento (ROI) que o site pode proporcionar em termos de aumento de vendas, visibilidade da marca, e eficiência operacional.</p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-10">
+            <h3 class="mt-5 mb-2 h2 text-primary">Onde a Publinet atua como uma empresa de criação de sites?</h3>
+            <ul class="list-group">
+                <li class="list-group-item text-secondary border-0">São Paulo</li>
+                <li class="list-group-item text-secondary border-0">Guarulhos</li>
+                <li class="list-group-item text-secondary border-0">São Bernardo do Campo</li>
+                <li class="list-group-item text-secondary border-0">Santo André</li>
+                <li class="list-group-item text-secondary border-0">Osasco</li>
+                <li class="list-group-item text-secondary border-0">São Caetano do Sul</li>
+                <li class="list-group-item text-secondary border-0">Diadema</li>
+                <li class="list-group-item text-secondary border-0">Carapicuíba</li>
+                <li class="list-group-item text-secondary border-0">Itaquaquecetuba</li>
+                <li class="list-group-item text-secondary border-0">Mogi das Cruzes</li>
+                <li class="list-group-item text-secondary border-0">Mauá</li>
+                <li class="list-group-item text-secondary border-0">Taboão da Serra</li>
+                <li class="list-group-item text-secondary border-0">Barueri</li>
+                <li class="list-group-item text-secondary border-0">Suzano</li>
+                <li class="list-group-item text-secondary border-0">Itapecerica da Serra</li>
+                <li class="list-group-item text-secondary border-0">Ferraz de Vasconcelos</li>
+                <li class="list-group-item text-secondary border-0">Cotia</li>
+                <li class="list-group-item text-secondary border-0">Franco da Rocha</li>
+                <li class="list-group-item text-secondary border-0">Francisco Morato</li>
+                <li class="list-group-item text-secondary border-0">Poá</li>
+                <li class="list-group-item text-secondary border-0">Itapevi</li>
+                <li class="list-group-item text-secondary border-0">São Lourenço da Serra</li>
+                <li class="list-group-item text-secondary border-0">Embu das Artes</li>
+                <li class="list-group-item text-secondary border-0">Santana de Parnaíba</li>
+                <li class="list-group-item text-secondary border-0">Itaquaquecetuba</li>
+            </ul>
+            <img src="images/Ed.png" class="img-fluid mt-5 mb-5" alt="Edmilson Ribeiro">
+        </div>
+    </div>
+</div>
 
 <?php include_once("footer.php"); ?>
